022269481410656c5e0377cf3642734adf374579
m0hit18o2 <mohitabinav03@gmail.com>
2026-07-31 00:10:27 +0000

    Analytics and performance addition

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import Sidebar from "./components/Sidebar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +30,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><Sidebar />{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Sidebar />
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
